36d40d6f3aeb411d3906c426ee088a53d73ddbf5
Makoto Nihei <nihemak@gmail.com>
2018-10-01 23:42:01 +0900

    Move todo detail to TodoDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoDetailComponent
   ],
   imports: [
     BrowserModule,
