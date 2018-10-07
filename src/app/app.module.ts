da10cfe5072faccd351d59078b40b98573fda8ba
Makoto Nihei <nihemak@gmail.com>
2018-10-07 12:21:48 +0900

    Remove dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
-import { TodoDetailComponent } from './todo-detail/todo-detail.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { TodoEditorComponent } from './todo-editor/todo-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    TodoDetailComponent,
-    DashboardComponent,
     TodoEditorComponent
   ],
   imports: [
