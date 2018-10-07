baad6fdc164b562ee82465780a95bbd8e277776d
Makoto Nihei <nihemak@gmail.com>
2018-10-07 12:24:06 +0900

    ng lint --fix

--- src/app/todo-editor/todo-editor.component.ts
+++ src/app/todo-editor/todo-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Todo } from '../todo';
 import { TodoService } from '../todo.service';
