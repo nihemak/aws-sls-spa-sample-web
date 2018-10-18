12fe72241989f65c89fe0cc14402f58b1c63cee7
Makoto Nihei <nihemak@gmail.com>
2018-10-19 00:25:38 +0900

    Move TODOS to MockWebApiService

--- src/app/mock-web-api.service.ts
+++ src/app/mock-web-api.service.ts
@@ -3,13 +3,83 @@ import { Injectable } from '@angular/core';
 import { InMemoryDbService, ParsedRequestUrl, RequestInfoUtilities } from 'angular-in-memory-web-api';
 
 import { Todo } from './todo';
-import { TODOS } from './mock-todos';
 import * as UUID from 'uuid';
 
 @Injectable()
 export class MockWebApiService implements InMemoryDbService {
   private api: any = {
-    todos: TODOS
+    todos: [
+      {
+        id: 'bc55004d-6bf3-45ba-a785-a2c2b62b24d8',
+        text: 'test todo1',
+        checked: false,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      },
+      {
+        id: '458b67c1-9263-4827-b0bf-6cf406a38c70',
+        text: 'test todo2',
+        checked: true,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      },
+      {
+        id: 'd2efc133-0f4f-4cbe-9c72-155b4efda4f0',
+        text: 'test todo3',
+        checked: false,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      },
+      {
+        id: '6814e207-3ab0-41ce-92b3-174a2aa52b5d',
+        text: 'test todo4',
+        checked: false,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      },
+      {
+        id: '0a8957bb-8268-4e80-874d-6cdb13cbfe06',
+        text: 'test todo5',
+        checked: false,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      },
+      {
+        id: '11e2dcb6-11b9-41ad-b730-acca343b42cb',
+        text: 'test todo6',
+        checked: false,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      },
+      {
+        id: '45daf5ca-e637-4649-a3ee-b88d8a24043b',
+        text: 'test todo7',
+        checked: false,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      },
+      {
+        id: 'e9b606c8-435f-4b0e-906e-c67be0283090',
+        text: 'test todo8',
+        checked: false,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      },
+      {
+        id: 'eb87bfce-d86f-40cb-8a2f-22bedd019f33',
+        text: 'test todo9',
+        checked: false,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      },
+      {
+        id: '7e566865-f9d8-4091-984d-69a7af48ab43',
+        text: 'test todo0',
+        checked: false,
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime()
+      }
+    ]
   }
 
   public createDb(): any {
