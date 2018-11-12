2d31021ea5c02736badfde812864946fb3336647
Makoto Nihei <nihemak@gmail.com>
2018-11-13 00:38:58 +0900

    Add amplify setting to environment

--- bin/pre_build.sh
+++ bin/pre_build.sh
@@ -3,13 +3,29 @@
 cat << EOS > ./src/environments/environment.ts
 export const environment = {
   production: false,
-  apiBaseUrl: '${API_BASE_URL}'
+  amplify: {
+    Auth: {
+      region: 'ap-northeast-1',
+      userPoolId: '${USER_POOL_ID}',
+      userPoolWebClientId: '${USER_POOL_CLIENT_ID}'
+    }
+  },
+  apiBaseUrl: '${API_BASE_URL}',
+  localstorageBaseKey: 'CognitoIdentityServiceProvider.${USER_POOL_CLIENT_ID}.'
 };
 EOS
 
 cat << EOS > ./src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiBaseUrl: '${API_BASE_URL}'
+  amplify: {
+    Auth: {
+      region: 'ap-northeast-1',
+      userPoolId: '${USER_POOL_ID}',
+      userPoolWebClientId: '${USER_POOL_CLIENT_ID}'
+    }
+  },
+  apiBaseUrl: '${API_BASE_URL}',
+  localstorageBaseKey: 'CognitoIdentityServiceProvider.${USER_POOL_CLIENT_ID}.'
 };
 EOS
