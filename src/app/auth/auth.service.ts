b8ac4b80293b62fe4b9c2f94ddebe55f6135a06c
Makoto Nihei <nihemak@gmail.com>
2018-11-25 19:35:36 +0900

    Add auth dummy mode

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,10 +5,20 @@ import { catchError, map, tap } from 'rxjs/operators';
 import Amplify, { Auth } from 'aws-amplify';
 import { environment } from './../../environments/environment';
 
+export interface IAuthService {
+  signUp(email: string, password: string): Observable<any>;
+  confirmSignUp(email: string, code: string): Observable<any>;
+  signIn(email: string, password: string): Observable<any>;
+  getData(): Observable<any>;
+  getIdToken(): Promise<string>;
+  isAuthenticated(): Observable<boolean>;
+  signOut(): void;
+}
+
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements IAuthService {
   public loggedIn: BehaviorSubject<boolean>;
   password!: String;
 
@@ -73,5 +83,57 @@ export class AuthService {
         },
         error => console.log(error)
       );
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MockAuthService implements IAuthService {
+  public loggedIn: BehaviorSubject<boolean>;
+  password!: String;
+  idSignIn = false;
+
+  constructor(
+    private router: Router
+  ) {
+    this.loggedIn = new BehaviorSubject<boolean>(false);
+  }
+
+  public signUp(_email: string, password: string): Observable<any> {
+    this.password = password;
+
+    return of(true);
+  }
+
+  public confirmSignUp(_email: string, _code: string): Observable<any> {
+    return of(true);
+  }
+
+  public signIn(_email: string, _password: string): Observable<any> {
+    this.idSignIn = true;
+    return of([])
+      .pipe(
+        tap(() => this.loggedIn.next(true))
+      );
+  }
+
+  public getData(): Observable<any> {
+    return of([]);
+  }
+
+  public getIdToken(): Promise<string> {
+    return Promise.resolve('dummyToken');
+  }
+
+  public isAuthenticated(): Observable<boolean> {
+    this.loggedIn.next(this.idSignIn);
+    return of(this.idSignIn);
+  }
+
+  public signOut(): void {
+    this.idSignIn = false;
+    this.loggedIn.next(this.idSignIn);
+    this.router.navigate(['/login']);
   }
 }
